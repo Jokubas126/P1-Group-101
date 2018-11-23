6f5dc3f0ac6dcc3c268a047d842f27e7cd3353f3
Jokubas <jokubas126@users.noreply.github.com>
2018-11-23 09:49:37 +0100

    Update Movement.cs

--- Leap Motion Game creation 1/Assets/Scripts/Movement.cs
+++ Leap Motion Game creation 1/Assets/Scripts/Movement.cs
@@ -4,49 +4,41 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-    public Rigidbody player;
-    public Transform playermove;
+    public Rigidbody player; //for adding force
+    public Transform playermove; //for constant movement and rotation
 
-    public float push = 30f;
-    public float velocity = 0.3f;
-    //float tempvelocity;
-    public float stop = 0.35f;
-    public float constantspeed = 0.3f;
+    public float push; // the size of the push when extending fingers
+    public float velocity; // current velocity
+    public float maxvelocity;
 
-    public float turn = 30f;
-    public float angle = 2f;
+    public float stop; //speed of stopping 
+    public float constantspeed; // speed of the plane, when nothing is being done
 
-    bool active = false;
+    public float angle;
+    
 	// Update is called once per frame
 	void Update () {
-        angle = 2f;
-        if (active == true)
+
+
+        if (velocity > 0.3)
         {
-            //for (float i = angle; i <= turn; i++)
-           // {
-                playermove.Rotate(0, turn * Time.deltaTime, 0);
-            //}
-            active = !active;
+            velocity -= stop * Time.deltaTime;
+            playermove.Translate(Vector3.forward * velocity);
+            playermove.Rotate(Vector3.up * velocity * angle * Time.deltaTime);
         }
-
-
-        
-        if (velocity >= 0.3)
+        else
         {
-            velocity = velocity - (stop * Time.deltaTime);
-            playermove.Translate(0, 0, velocity);
+            playermove.Translate(Vector3.forward * constantspeed * Time.deltaTime);
+            playermove.Rotate(Vector3.up * velocity * angle);
         }
-        else playermove.Translate(0, 0, constantspeed * Time.deltaTime);
-
     }
 
     public void PrintActivateMesssage()
-    {   
-        if (velocity < 1)
+    {
+        if (velocity < maxvelocity)
         {
             velocity += push * Time.deltaTime;
-            player.AddForce(0, 0, velocity);
-            active = !active;
+            player.AddForce(Vector3.forward * velocity, ForceMode.VelocityChange);
         }
     }
     public void PrintDeactivateMesssage(){}
